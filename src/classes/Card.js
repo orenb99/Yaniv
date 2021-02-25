1e6a40404ae02340432bafb66dde302b70a76c82
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-02-25 18:09:02 +0200

    starting

---
+++ src/classes/Card.js
@@ -0,0 +1,20 @@
+class Card{
+    constructor(suit,rank,isJoker){
+        this.suit=suit;
+        this.rank=rank;
+        this.isJoker=isJoker;
+    }
+
+    flip(face){
+        if(face==="down"){
+
+        }
+        else if(face==="up"){
+
+        }
+
+
+    }
+
+}
+module.exports= Card;
---
+++ src/classes/Deck.js
@@ -0,0 +1,7 @@
+const Card = require("./Card.js")
+class Deck{
+    constructor(){}
+}
+
+let c=new Card(1,2,false)
+console.log(c);
