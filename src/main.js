bb46a06fe8359e2680fdd2b5f37f860da5940114
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-02-27 00:47:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,23 +5,24 @@ function main() {
     const player2=document.getElementById("player2-div");
     const player3=document.getElementById("player3-div");
     const player4=document.getElementById("player4-div");
-    const tableDiv=document.getElementById("table-div");
-    const pileDiv=document.getElementById("pile-div");
+    const tableSpan=document.getElementById("table-div").getElementsByTagName("span")[0];
+    const pileSpan=document.getElementById("pile-div").getElementsByTagName("span")[0];
 
-    //objects
+    //objects and variables
+    let turnCounter=1;
     const tableDeck=createTableDeck();
     const pileDeck=new PileDeck();
-    //pileDeck.addCard(new Card("Spades","ace",false));
-    appendDeck(pileDeck,pileDiv.getElementsByTagName("span")[0]);
-    appendDeck(tableDeck,tableDiv.getElementsByTagName("span")[0]);
-    tableDiv.getElementsByTagName("span")[0].addEventListener("click",()=>{
+
+    //appends
+    appendDeck(pileDeck,pileSpan);
+    appendDeck(tableDeck,tableSpan);
+
+    //event listeners
+    tableSpan.addEventListener("click",()=>{
         moveCard(tableDeck,pileDeck,tableDeck.cards[0]);
-        appendDeck(pileDeck,pileDiv.getElementsByTagName("span")[0])
-        appendDeck(tableDeck,tableDiv.getElementsByTagName("span")[0])
+        appendDeck(pileDeck,pileSpan)
+        appendDeck(tableDeck,tableSpan)
     })
-    console.log(pileDeck)
-
-
     
 }
 main();
